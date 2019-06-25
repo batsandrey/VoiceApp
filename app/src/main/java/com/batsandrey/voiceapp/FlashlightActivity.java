adf013376a86968e6a0089a15334e55379822f8b
batsandrey <batsandrey.it@gmail.com>
2019-06-25 13:58:41 +0300

    adding flashlight provider and basically realize full brightness

---
+++ app/src/main/java/com/batsandrey/voiceapp/FlashlightActivity.java
@@ -0,0 +1,13 @@
+package com.batsandrey.voiceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashlightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashlight);
+    }
+}
---
+++ app/src/main/java/com/batsandrey/voiceapp/FlashlightProvider.java
@@ -0,0 +1,4 @@
+package com.batsandrey.voiceapp;
+
+public class FlashlightProvider {
+}
